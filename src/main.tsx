b3b3205625e1cb4eb2ad4ed07431785f2eddd3e4
ikeawesom <ikelimjl@gmail.com>
2024-04-18 23:31:43 +0800

    wrapped snacks provider

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { ReportProvider } from "./components/contexts/ReportContext.tsx";
+import { SnacksProvider } from "./components/contexts/NightSnackContext.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ReportProvider>
-      <App />
+      <SnacksProvider>
+        <App />
+      </SnacksProvider>
     </ReportProvider>
   </React.StrictMode>
 );
