fd111390983958fd10516ed6bdb0c6b3984d4b51
ikeawesom <ikelimjl@gmail.com>
2024-04-18 23:30:58 +0800

    added new constants and types for Snacks

--- src/constants.ts
+++ src/constants.ts
@@ -6,6 +6,27 @@ export type UtilityType = {
 
 export type DayType = { day: number; month: number; year: number };
 
+export type DailyRationType = {
+  state: boolean;
+  or: boolean;
+  qty: { [mealType: string]: number };
+};
+
+export type DailySnackType = {
+  state: boolean;
+  qty: { [snackType: string]: number };
+};
+
+export type WeeklyRationType = {
+  [day: string]: { [meal: string]: DailyRationType };
+};
+
+export type WeeklySnackType = {
+  [day: string]: DailySnackType;
+};
+
+export const MEALS = ["Breakfast", "Lunch", "Dinner"];
+
 export const DefaultRationQty = {
   "Non-Muslim": 0,
   Muslim: 0,
@@ -14,13 +35,12 @@ export const DefaultRationQty = {
   "Special Diet": 0,
 };
 
-export type DailyRationType = {
-  state: boolean;
-  or: boolean;
-  qty: { [mealType: string]: number };
+export const DefaultSnacksQty = {
+  Carbs: 0,
+  Protein: 0,
+  "Night 1": 0,
+  "Night 2": 0,
 };
-
-export const MEALS = ["Breakfast", "Lunch", "Dinner"];
 
 export const DAILY_RATION = {
   Breakfast: {
@@ -40,9 +60,10 @@ export const DAILY_RATION = {
   },
 } as { [meal: string]: DailyRationType };
 
-export type WeeklyRationType = {
-  [day: string]: { [meal: string]: DailyRationType };
-};
+export const DAILY_SNACKS = {
+  state: false,
+  qty: DefaultSnacksQty,
+} as DailySnackType;
 
 export const WEEKLY_RATION = {
   Monday: DAILY_RATION,
@@ -53,3 +74,13 @@ export const WEEKLY_RATION = {
   Saturday: DAILY_RATION,
   Sunday: DAILY_RATION,
 } as WeeklyRationType;
+
+export const WEEKLY_SNACKS = {
+  Monday: DAILY_SNACKS,
+  Tuesday: DAILY_SNACKS,
+  Wednesday: DAILY_SNACKS,
+  Thursday: DAILY_SNACKS,
+  Friday: DAILY_SNACKS,
+  Saturday: DAILY_SNACKS,
+  Sunday: DAILY_SNACKS,
+} as WeeklySnackType;
